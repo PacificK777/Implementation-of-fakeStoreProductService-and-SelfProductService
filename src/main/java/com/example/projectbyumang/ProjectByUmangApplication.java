1b55ab8f098a6307c542ad0f8eb31b31c852cb58
PacificK777 <prashantk0008@gmail.com>
2024-05-26 22:33:30 +0530

    create empty repo

---
+++ src/main/java/com/example/projectbyumang/ProjectByUmangApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectbyumang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectByUmangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectByUmangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectbyumang/ProjectByUmangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectbyumang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectByUmangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
